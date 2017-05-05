093acc68e90de9fb28234b6ca7c77fdcb9fb9f9d
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-05 17:23:04 +0700

    New Concept

--- README.md
+++
@@ -1,5 +0,0 @@
-# Facebook_Interactive_Chat
-## Cara Pakai :
-1. Rename config.php.dist menjadi config.php
-2. Ubah setting config.php dengan facebook bot
-3. Jalankan cli.php
--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-header('content-type:text/plain');
-http_response_code(200);;
-ini_set("max_execution_time", false);
-ini_set("memory_limit", "3072M");
-ignore_user_abort(true);
-set_time_limit(0);
-error_reporting(0);
-for ($i=0;$i<2;$i++) {
-    shell_exec("php -q cli.php");
-}
-shell_exec("rm -rf error_log");
+/**
+*	silent is golden
+*/
