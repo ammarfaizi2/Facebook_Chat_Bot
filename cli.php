d50d8cacc0a40ccc8d76f33175408428e9127fff
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-05 20:29:57 +0700

    Rebuild Facebook

--- cli.php
+++ cli.php
@@ -1,10 +1,21 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/config.php';
 use System\AI;
+use System\Facebook;
+use System\ChatController;
 
 define('data', __DIR__.'/data');
+define('fb_data',data.'/fb_data');
 is_dir(data) or mkdir(data);
+is_dir(fb_data) or mkdir(fb_data);
+
+
+/* debugging here
 $a = new AI();
 $st = $a->prepare("translate hello");
 $st->execute();
 var_dump($st->fetch_reply());
+//*/
+
+$fb = new Facebook($config['email'],$config['pass'],$config['user'],$config['token']);
