1cf883904a82f6f6591338c68557439f578c6955
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-05 23:15:34 +0700

    Fix array pos

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $config = array(
     'name'    =>    'Ammar Kazuhiko Kanazawa',
     'user'    =>    'ammarfaizi93',
-    'email'    =>    'ammarfaizi93@gmail.com',
+    'email'   =>    'ammarfaizi93@gmail.com',
     'pass'    =>    '858869123aaa',
-    'token'    =>    ''
+    'token'   =>    ''
 );
