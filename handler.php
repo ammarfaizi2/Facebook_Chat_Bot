c9a967818671059c12a4b90edd65e70206fd16c0
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-25 00:21:28 +0700

    PHP CS Fixer

--- handler.php
+++ handler.php
@@ -2,6 +2,6 @@
 set_time_limit(0);
 ignore_user_abort(true);
 http_response_code(200);
-for ($i=0; $i < 5; $i++) { 
-	shell_exec('php -q cli.php');
+for ($i=0; $i < 5; $i++) {
+    shell_exec('php -q cli.php');
 }
