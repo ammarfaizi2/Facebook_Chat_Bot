5a37b07b061d34a1cca91541c45417d825eb0760
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-09 14:27:41 +0700

    Update config and autoload files

--- src/Bot/BotFacebook.php
+++ src/Bot/BotFacebook.php
@@ -5,8 +5,28 @@ namespace Bot;
 /**
  * @author Ammar Faizi
  * @version 2.0.1
+ * @license 
  */
+
+use AI\AI;
+use Facebook\Facebook;
+
 class BotFacebook
 {
+	const VERSION = "2.0.1";
 
+	/**
+	 * @var Facebook\Facebook
+	 */
+	private $fb;
+
+	/**
+	 * @param string $email
+	 * @param string $pass
+	 * @param string $user
+	 */
+	public function __construct($email, $pass, $user)
+	{
+		$this->fb = new Facebook($email, $pass, $user);
+	}
 }
